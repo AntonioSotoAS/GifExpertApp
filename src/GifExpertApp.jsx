47c08024ec67db46378f6a8016ce86d072a28942
Arturo Antonio Montejo Soto <121409768+AntonioSotoAS@users.noreply.github.com>
2023-05-17 09:30:25 -0500

    proyecto configurado

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* input */}
+
+            {/* Listado de Gif */}
+                {/* Gift Item */}
+        </>
+    )
+}
